2e0309cf6bd8cc86438c07b37d52c64ec4c6b736
sohelakhtar23 <sohelakhtar23@gmail.com>
2021-06-24 17:35:54 +0545

    Dark/ light theme Feature completed

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './dark_mode.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => ThemeNotifier(),
+      child: Consumer<ThemeNotifier>(
+        builder: (context, notifier, child) {
+          return MaterialApp(
+            title: 'Flutter Theme Provider',
+            theme: notifier.darkTheme ? kdark : klight,
+            home: HomePage(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Theme Provider'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Consumer<ThemeNotifier>(
+            builder: (context, notifier, child) => SwitchListTile(
+              title: Text("Dark Mode"),
+              onChanged: (val) {
+                notifier.toggleTheme();
+              },
+              value: notifier.darkTheme,
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text("This is just a list tile on a card."),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
